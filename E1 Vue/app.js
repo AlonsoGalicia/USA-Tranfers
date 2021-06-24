62603b19de5afdc1f8030dba57c2ddc0b60b8f1b
Alonso Galicia <40242435+AlonsoGalicia@users.noreply.github.com>
2021-06-24 12:46:19 -0500

    Creación de computed para totales

--- E1 Vue/app.js
+++ E1 Vue/app.js
@@ -4,25 +4,39 @@ const app = Vue.createApp({
       enteredProduct: "",
       enteredNumberOfProduct: "",
       products: [],
-      total: 0,
+      total: "",
     };
+  },
+  computed: {
+    sumTotal() {
+      if (this.products.length === 0) {
+        return (this.total = 0);
+      } else {
+        return (this.total = this.products
+          .map((col) => col[1])
+          .reduce((a, b) => a + b));
+      }
+    },
+    validateTotal() {
+      return {
+        "h3 alert alert-danger": this.total === 0,
+        "h3 alert alert-success": this.total > 0,
+      };
+    },
   },
   methods: {
     addProduct() {
       if (this.enteredProduct !== "" && this.enteredNumberOfProduct !== "") {
         this.products.push([this.enteredProduct, this.enteredNumberOfProduct]);
-        this.total = this.total + this.enteredNumberOfProduct;
         this.enteredProduct = "";
         this.enteredNumberOfProduct = "";
       }
     },
     removeProduct(idx) {
-      this.total = this.total - this.products[idx][1];
       this.products.splice(idx, 1);
     },
     removeAll() {
       this.products = [];
-      this.total = 0;
     },
   },
 });
